275a8b8c3d8b79dbf1f61746d50227ded771ecba
Patinya Yongyai <patinya.y@ku.th>
2017-10-20 14:34:24 +0700

    test handleMessage

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function(req, res) {
                 continue
             }
 
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, handleMessage(text.substring(0, 200)))
         }
         if (event.postback) {
             let text = JSON.stringify(event.postback)
