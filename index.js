bdeec26901a3ec80b0ff5eab86eedcf09a99ed12
Patinya Yongyai <patinya.y@ku.th>
2017-10-25 17:32:03 +0700

    add state of bot and add more complexe condition

--- index.js
+++ index.js
@@ -80,23 +80,31 @@ app.post('/webhook/', function(req, res) {
                 console.log(maxConfidence);
                 console.log(entities);
                 if (maxConfidence > 0.8 && entities === "greeting") {
-                    stateConversation = "greeting";
+                    stateConversation = entities;
                     sendTextMessage(sender, "สวัสดีครับ...คุณต้องการดูดวงกับเรามั้ย?")
-                } else if (maxConfidence > 0.8 && entities === "accept" && stateConversation === "greeting") {
-                    stateConversation = "accept";
-                    sendTextMessage(sender, "งั้นก็ส่งวันเกิดของคุณมาเลย!! (ตัวอย่าง 28 มิถุนายน 2540)")
+                } else if (maxConfidence > 0.8 && entities === "accept") {
+                    stateConversation = entities;
+                    if (stateConversation === "greeting") {
+                        sendTextMessage(sender, "งั้นก็ส่งวันเกิดของคุณมาเลย!! (ตัวอย่าง 28 มิถุนายน 2540)")
+                    } else if (stateConversation === "birthday") {
+                        sendTextMessage(sender, "ดวงของคุณช่วงนี้คือ...")
+                    }
                 } else if (maxConfidence > 0.8 && entities === "cancel") {
-                    stateConversation = "cancel";
-                    sendTextMessage(sender, "ไม่อยากดูจริงๆหรอ?")
+                    stateConversation = entities;
+                    if (stateConversation === "date" || stateConversation === "month" || stateConversation === "year") {
+                        sendTextMessage(sender, "งั้นก็ส่งวันเกิดของคุณมาเลย!! (ตัวอย่าง 28 มิถุนายน 2540)")
+                    } else {
+                        sendTextMessage(sender, "ไม่อยากดูจริงๆหรอ?")
+                    }
                 } else if (maxConfidence > 0.8 && entities === "bye") {
-                    stateConversation = "bye";
+                    stateConversation = entities;
                     sendTextMessage(sender, "แล้วเจอกันจ้าา")
                 } else if (maxConfidence > 0.8 && entities === "askDetail") {
-                    stateConversation = "askDetail";
+                    stateConversation = entities;
                     sendTextMessage(sender, "ตอนนี้เราสามารถดูดวงได้แค่ตามวันเกิดเอง")
                 } else if (maxConfidence > 0.8 && (entities === "date" || entities === "month" || entities === "year") && stateConversation === "accept") {
-                    stateConversation = "birthday";
-                    sendTextMessage(sender, "วันเกิดของคุณคือ วันที่ " + tempBirthday[0] + " เดือน" + tempBirthday[1] + " " + tempBirthday[2] + " ใช่หรือไม่?")
+                    stateConversation = entities;
+                    sendTextMessage(sender, "คุณเกิดวันที่ " + tempBirthday[0] + " " + tempBirthday[1] + " " + tempBirthday[2] + " ใช่หรือไม่?")
                 } else {
                     sendTextMessage(sender, "กรุณาใส่ข้อความให้ถูกต้องด้วยครับ")
                     sendTextMessage(sender, "ตอนนี้เราสามารถดูดวงได้แค่ตามวันเกิดเอง")
