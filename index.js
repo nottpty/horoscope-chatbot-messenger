edb972a40944ad8f8076d7ec8303684c49db2c85
Patinya Yongyai <patinya.y@ku.th>
2017-10-27 17:51:15 +0700

    install get-json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const getJSON = require('get-json')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -54,12 +55,12 @@ let stateConversation = "";
 let realBirthday = [];
 let predictionJSON = [];
 let monthJSON = [];
-$.getJSON("prediction.json", function(json) {
-    predictionJSON = json;
-});
-$.getJSON("month.json", function(json) {
-    month = json;
-});
+getJSON('prediction.json', function(error, response) {
+    predictionJSON = response.result
+})
+getJSON('month.json', function(error, response) {
+    monthJSON = response.result
+})
 
 function findNumberPrediction(resultBirthday) {
     let convertToStr = n + "";
