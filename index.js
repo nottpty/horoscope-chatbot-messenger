e2df585920c06c7caec4f1cc51f95d0d69cad0f8
Patinya Yongyai <patinya.y@ku.th>
2017-11-01 11:02:23 +0700

    move Wit access token to heroku

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/webhook/', function(req, res) {
 const { Wit, log } = require('node-wit');
 
 const client = new Wit({
-    accessToken: 'UH7OOY34YWNIK5IMUJO7NRVGKHENN2UG',
+    accessToken: process.env.WIT_ACCESS_TOKEN,
     // logger: new log.Logger(log.DEBUG) // optional
 });
 
